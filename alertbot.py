a766031d4bb99f0749978c7c0067c5edde619906
raydarnet <hansen.wu.85@gmail.com>
2017-12-18 10:09:34 -0800

    fix: incorrect order of commands

--- alertbot.py
+++ alertbot.py
@@ -69,8 +69,8 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_polling()
-    updater.idle()
     logger.info('bot is up and running')
+    updater.idle()
 
 
 if __name__ == '__main__':
